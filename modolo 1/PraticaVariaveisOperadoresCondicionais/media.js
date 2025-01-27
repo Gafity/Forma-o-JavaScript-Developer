c36a7c005f58a66d5018033995bc56a5108fb54a
Gafity <gafityr@gmail.com>
2025-01-27 18:52:21 -0300

    Iniciando as Praticas

---
+++ modolo 1/PraticaVariaveisOperadoresCondicionais/media.js
@@ -0,0 +1,7 @@
+/* Faça um algoritmo que dado 3 notas tiradas por um aluno em um semestre da faculdade calcule 
+e imprima sua média e sua classificacao conforme a tabela a baixo
+
+- menor que 5 = reprovado
+- entre 5 e 7 = recuperacao
+- media maior que 7 = aprovado
+*/
