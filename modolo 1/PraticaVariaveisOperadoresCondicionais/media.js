993b966cb60a097905ba0ccbde299d2d58bcb81b
Gafity <gafityr@gmail.com>
2025-01-28 08:01:03 -0300

    commit

--- modolo 1/PraticaVariaveisOperadoresCondicionais/media.js
+++ modolo 1/PraticaVariaveisOperadoresCondicionais/media.js
@@ -5,3 +5,13 @@ e imprima sua média e sua classificacao conforme a tabela a baixo
 - entre 5 e 7 = recuperacao
 - media maior que 7 = aprovado
 */
+
+// Notas do semestre
+let primeiraNota = 10;
+let segundaNota = 4;
+let terceiraNota = 8;
+
+// Media do semestre
+let mediaSemestre = (primeiraNota + segundaNota + terceiraNota) / 3
+
+if (mediaSemestre > 7)
