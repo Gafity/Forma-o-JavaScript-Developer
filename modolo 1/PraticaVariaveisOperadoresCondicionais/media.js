a71dbc1fe108126cbd7c86bfd264c2f88d0c2aee
Gabriel Gama <gafityr@gmail.com>
2025-01-29 10:41:06 -0300

    finalizacao modulo 1

--- modolo 1/PraticaVariaveisOperadoresCondicionais/media.js
+++ modolo 1/PraticaVariaveisOperadoresCondicionais/media.js
@@ -7,11 +7,17 @@ e imprima sua média e sua classificacao conforme a tabela a baixo
 */
 
 // Notas do semestre
-let primeiraNota = 10;
-let segundaNota = 4;
-let terceiraNota = 8;
+const primeiraNota = 10;
+const segundaNota = 4;
+const terceiraNota = 8;
 
 // Media do semestre
-let mediaSemestre = (primeiraNota + segundaNota + terceiraNota) / 3
+const mediaSemestre = (primeiraNota + segundaNota + terceiraNota) / 3
 
-if (mediaSemestre > 7)
+if (mediaSemestre > 7){
+    console.log(`Parabéns, você foi aprovado! | Media: ${mediaSemestre.toFixed(2)}`);
+} else if(mediaSemestre >= 5 && mediaSemestre < 7){
+    console.log(`Se esforce mais, voce esta de recuperacao! | Media: ${mediaSemestre.toFixed(2)}`);
+} else {
+    console.log('Lamentamos, mas voce foi reprovado!');
+}
